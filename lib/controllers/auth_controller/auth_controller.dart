25ef64918adb3e243fe66425e96902074732d08f
Mizael Eduardo <eduardo_boyfla@hotmail.com>
2022-10-18 16:18:12 -0300

    Add to android in firebase and init functions

--- lib/controllers/auth_controller/auth_controller.dart
+++ lib/controllers/auth_controller/auth_controller.dart
@@ -1,5 +1,14 @@
+import 'dart:io';
 import 'package:get/get_state_manager/get_state_manager.dart';
 
 class AuthController extends GetxController {
-  //
+  // Registering the user
+  void registerUser(
+    String username,
+    String email,
+    String password,
+    File? imge,
+  ) {
+    //
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,20 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:tiktok_clone/constants/constants.dart';
 import 'package:tiktok_clone/views/screens/auth/login_screen/login_screen.dart';
 import 'package:tiktok_clone/views/screens/auth/signup_screen/signup_screen.dart';
 
+import 'controllers/auth_controller/auth_controller.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp().then(
+    (value) => {
+      Get.put(AuthController()),
+    },
+  );
   runApp(const MyApp());
 }
 
