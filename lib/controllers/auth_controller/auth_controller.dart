9ab5d8615580582c28ace45c0f0d9d14ae85b266
Mizael Eduardo <eduardo_boyfla@hotmail.com>
2022-10-19 16:14:45 -0300

    Create Functions Upload to firebase storage, Registering the user, Save out user to our ath and firebase firestore. And create User model

--- lib/controllers/auth_controller/auth_controller.dart
+++ lib/controllers/auth_controller/auth_controller.dart
@@ -1,14 +1,63 @@
 import 'dart:io';
-import 'package:get/get_state_manager/get_state_manager.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:get/get.dart';
+import 'package:tiktok_clone/constants/constants.dart';
+import 'package:tiktok_clone/models/user/user.dart' as model;
 
 class AuthController extends GetxController {
+  // Upload to firebase storage
+  Future<String> _uploadToStorage(File image) async {
+    Reference ref = firebaseStorage
+        .ref()
+        .child('profilePics')
+        .child(firebaseAuth.currentUser!.uid);
+
+    UploadTask uploadTask = ref.putFile(image);
+    TaskSnapshot snap = await uploadTask;
+    String downloadUrl = await snap.ref.getDownloadURL();
+    return downloadUrl;
+  }
+
   // Registering the user
   void registerUser(
     String username,
     String email,
     String password,
-    File? imge,
-  ) {
-    //
+    File? image,
+  ) async {
+    try {
+      if (username.isNotEmpty &&
+          email.isNotEmpty &&
+          password.isNotEmpty &&
+          image != null) {
+        // Save out user to our ath and firebase firestore
+        UserCredential cred = await firebaseAuth.createUserWithEmailAndPassword(
+          email: email,
+          password: password,
+        );
+        String downloadUrl = await _uploadToStorage(image);
+        model.User user = model.User(
+          email: email,
+          name: username,
+          profilePhoto: downloadUrl,
+          uid: cred.user!.uid,
+        );
+        await firestore
+            .collection('users')
+            .doc(cred.user!.uid)
+            .set(user.toJson());
+      } else {
+        Get.snackbar(
+          'Error Creating Account',
+          'Please enter all the fields',
+        );
+      }
+    } catch (e) {
+      Get.snackbar(
+        'Error Creating Account',
+        e.toString(),
+      );
+    }
   }
 }
